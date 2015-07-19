4b60f3482d239b1f41f1cdaa6615d4771b78ac9f
Will <yellowstar1992@hotmail.com>
2015-07-19 21:56:58 +0800

    [Xing]: remove debug info

--- main.py
+++ main.py
@@ -51,8 +51,6 @@ class CL_spider:
         req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36')
         resp = urllib2.urlopen(req)
         subPage = resp.read()
-
-        print subPage
         
         movieList = re.findall(u'<tr.*?<h3.*?<a.*?href="(.*?)".*?target="_blank".*?>\[.*?\](.*?)</a>.*?</h3>.*?</tr>', subPage, re.S)
 
